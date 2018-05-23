a0870f90085c222ef534153ab75b520a2551ed59
Dylan Murray <dymurray@redhat.com>
2018-05-23 15:15:04 -0400

    Add exec command and refactor list (#5)
    * First pass at exec cmd - not functional
    * exec updates
    * Get provision working
    * Add parameter support
    * Update list and param selections

---
+++ cmd/bundle.go
@@ -0,0 +1,107 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/automationbroker/bundle-lib/bundle"
+	"github.com/automationbroker/bundle-lib/registries"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var Refresh bool
+
+var bundleCmd = &cobra.Command{
+	Use:   "bundle",
+	Short: "Interact with ServiceBundle images",
+	Long:  `Interact with and list ServiceBundles from configured registry adapters`,
+}
+
+var bundleListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List ServiceBundle images",
+	Long:  `List ServiceBundles from a registry adapter`,
+	Run: func(cmd *cobra.Command, args []string) {
+		listImages()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(bundleCmd)
+	bundleListCmd.Flags().BoolVarP(&Refresh, "refresh", "r", false, "refresh list of specs")
+	bundleCmd.AddCommand(bundleListCmd)
+}
+
+func updateCachedList(specs []*bundle.Spec) error {
+	viper.Set("Specs", specs)
+	viper.WriteConfig()
+	return nil
+}
+
+func getImages() ([]*bundle.Spec, error) {
+	var regConfigList []registries.Config
+	var regList []registries.Registry
+	var specList []*bundle.Spec
+	err := viper.UnmarshalKey("Registries", &regConfigList)
+	if err != nil {
+		fmt.Println("Error unmarshalling config: ", err)
+		return nil, err
+	}
+
+	authNamespace := ""
+	for _, config := range regConfigList {
+		registry, err := registries.NewRegistry(config, authNamespace)
+		if err != nil {
+			log.Error("Error from creating a NewRegistry")
+			log.Error(err)
+			return nil, err
+		}
+		regList = append(regList, registry)
+	}
+	for _, reg := range regList {
+		specs, count, err := reg.LoadSpecs()
+		if err != nil {
+			log.Errorf("registry: %v was unable to complete bootstrap - %v",
+				reg.RegistryName(), err)
+			return nil, err
+		}
+		log.Infof("Registry %v has %d bundles available from %d images scanned", reg.RegistryName(), len(specs), count)
+		specList = append(specList, specs...)
+	}
+
+	return specList, nil
+}
+
+func listImages() {
+	var specs []*bundle.Spec
+	err := viper.UnmarshalKey("Specs", &specs)
+	if err != nil {
+		fmt.Println("Error unmarshalling config: ", err)
+		return
+	}
+	if len(specs) > 0 && Refresh == false {
+		fmt.Println("Found specs already in config")
+		for _, s := range specs {
+			fmt.Printf("%v - %v\n", s.FQName, s.Image)
+		}
+		return
+	}
+
+	specs, err = getImages()
+	if err != nil {
+		fmt.Println("Error getting images")
+		return
+	}
+	fmt.Printf("specs: %v", specs)
+	err = updateCachedList(specs)
+	if err != nil {
+		fmt.Println("Error updating cache")
+		return
+	}
+
+	for _, s := range specs {
+		fmt.Printf("%v - %v\n", s.FQName, s.Image)
+	}
+}
---
+++ cmd/exec.go
@@ -0,0 +1,216 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/automationbroker/bundle-lib/bundle"
+	"github.com/automationbroker/bundle-lib/clients"
+	"github.com/automationbroker/bundle-lib/runtime"
+	"github.com/pborman/uuid"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//	log "github.com/sirupsen/logrus"
+)
+
+var execName string
+var execNamespace string
+
+var execCmd = &cobra.Command{
+	Use:   "exec",
+	Short: "Perform specified action on Service Bundles",
+	Long:  `Perform actions (Provision, Deprovision, Bind, Unbind) on Service Bundles`,
+}
+
+var execProvisionCmd = &cobra.Command{
+	Use:   "provision",
+	Short: "Provision ServiceBundle images",
+	Long:  `Provision ServiceBundles from a registry adapter`,
+	Run: func(cmd *cobra.Command, args []string) {
+		runBundle()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(execCmd)
+	execProvisionCmd.Flags().StringVarP(&execName, "name", "n", "", "Name of spec to provision")
+	execProvisionCmd.Flags().StringVarP(&execNamespace, "namespace", "p", "", "Namespace to provision bundle to")
+	execCmd.AddCommand(execProvisionCmd)
+}
+
+func runBundle() {
+	if execName == "" {
+		fmt.Println("Failed to find --name argument. No bundle selected")
+		return
+	}
+	if execNamespace == "" {
+		fmt.Println("Failed to find --namespace argument. No namespace selected")
+		return
+	}
+	specs := []*bundle.Spec{}
+	var targetSpec *bundle.Spec
+	targets := []string{execNamespace}
+	pn := fmt.Sprintf("bundle-%s", uuid.New())
+	viper.UnmarshalKey("Specs", &specs)
+	for _, s := range specs {
+		if s.FQName == execName {
+			targetSpec = s
+		}
+	}
+	if targetSpec == nil {
+		fmt.Printf("Didn't find supplied APB: %v\n", execName)
+		return
+	}
+	plan := selectPlan(targetSpec)
+	if plan.Name == "" {
+		fmt.Println("Did not find a selected plan")
+	} else {
+		fmt.Printf("Plan: %v\n", plan.Name)
+	}
+	params := selectParameters(plan)
+	extraVars, err := createExtraVars(execNamespace, &params, plan)
+	if err != nil {
+		fmt.Printf("Error creating extravars: %v\n", err)
+		return
+	}
+
+	labels := map[string]string{
+		"bundle-fqname":   targetSpec.FQName,
+		"bundle-action":   "provision",
+		"bundle-pod-name": pn,
+	}
+	ec := runtime.ExecutionContext{
+		BundleName: pn,
+		Targets:    targets,
+		Metadata:   labels,
+		Action:     "provision",
+		Image:      targetSpec.Image,
+		Account:    "apb",
+		Location:   execNamespace,
+		ExtraVars:  extraVars,
+	}
+	//	conf := runtime.Configuration{}
+	//	runtime.NewRuntime(conf)
+	k8scli, err := clients.Kubernetes()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := k8scli.Client.CoreV1().Pods(execNamespace).List(metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error getting list of pods: %v", err)
+		return
+	}
+	fmt.Printf("%v\n", len(pods.Items))
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   ec.BundleName,
+			Labels: ec.Metadata,
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  pn,
+					Image: ec.Image,
+					Args: []string{
+						ec.Action,
+						"--extra-vars",
+						ec.ExtraVars,
+					},
+					Env:             createPodEnv(ec),
+					ImagePullPolicy: "IfNotPresent",
+				},
+			},
+			RestartPolicy:      v1.RestartPolicyNever,
+			ServiceAccountName: ec.Account,
+		},
+	}
+	_, err = k8scli.Client.CoreV1().Pods(execNamespace).Create(pod)
+	if err != nil {
+		fmt.Printf("Failed to create pod: %v", err)
+	}
+	return
+}
+
+func selectPlan(spec *bundle.Spec) bundle.Plan {
+	var planName string
+	if len(spec.Plans) > 1 {
+		fmt.Printf("List of available plans:\n")
+		for _, plan := range spec.Plans {
+			fmt.Printf("name: %v\n", plan.Name)
+		}
+		fmt.Printf("Enter name of plan you'd like to deploy: ")
+		fmt.Scanln(&planName)
+	} else {
+		return spec.Plans[0]
+	}
+	for _, plan := range spec.Plans {
+		if plan.Name == planName {
+			return plan
+		}
+	}
+	return bundle.Plan{}
+}
+
+func selectParameters(plan bundle.Plan) bundle.Parameters {
+	params := bundle.Parameters{}
+	for _, param := range plan.Parameters {
+		var paramDefault string = ""
+		var paramInput string
+		if param.Default != nil {
+			paramDefault = param.Default.(string)
+		}
+		fmt.Printf("Enter value for parameter [%v], default: [%v]: ", param.Name, paramDefault)
+		fmt.Scanln(&paramInput)
+		if paramInput == "" {
+			paramInput = paramDefault
+		}
+		params.Add(param.Name, paramInput)
+	}
+	fmt.Printf("Params: %v\n", params)
+	return params
+}
+
+func createPodEnv(executionContext runtime.ExecutionContext) []v1.EnvVar {
+	podEnv := []v1.EnvVar{
+		v1.EnvVar{
+			Name: "POD_NAME",
+			ValueFrom: &v1.EnvVarSource{
+				FieldRef: &v1.ObjectFieldSelector{
+					FieldPath: "metadata.name",
+				},
+			},
+		},
+		v1.EnvVar{
+			Name: "POD_NAMESPACE",
+			ValueFrom: &v1.EnvVarSource{
+				FieldRef: &v1.ObjectFieldSelector{
+					FieldPath: "metadata.namespace",
+				},
+			},
+		},
+	}
+	return podEnv
+}
+
+func createExtraVars(targetNamespace string, parameters *bundle.Parameters, plan bundle.Plan) (string, error) {
+	var paramsCopy bundle.Parameters
+	if parameters != nil && *parameters != nil {
+		paramsCopy = *parameters
+	} else {
+		paramsCopy = make(bundle.Parameters)
+	}
+
+	if targetNamespace != "" {
+		paramsCopy["namespace"] = targetNamespace
+	}
+
+	paramsCopy["cluster"] = "openshift"
+	paramsCopy["_apb_plan_id"] = plan.Name
+	paramsCopy["_apb_service_instance_id"] = "1234"
+	paramsCopy["_apb_service_class_id"] = "1234"
+	extraVars, err := json.Marshal(paramsCopy)
+	return string(extraVars), err
+}
