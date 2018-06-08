0b8556e8869532839fe2d1dd0d5079afde3f91ef
Dylan Murray <dymurray@redhat.com>
2018-06-08 14:04:57 -0400

    Use json schema for param validation

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -18,11 +18,14 @@ package runner
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/automationbroker/bundle-lib/bundle"
 	"github.com/automationbroker/bundle-lib/clients"
 	"github.com/automationbroker/bundle-lib/runtime"
+	"github.com/lestrrat/go-jsschema/validator"
 	"github.com/pborman/uuid"
 	"github.com/spf13/viper"
 	"k8s.io/api/core/v1"
@@ -48,14 +51,20 @@ func RunBundle(action string, ns string, args []string) {
 		log.Errorf("Didn't find supplied APB: %v\n", bundleName)
 		return
 	}
+
 	plan := selectPlan(targetSpec)
 	if plan.Name == "" {
 		log.Warning("Did not find a selected plan")
 	} else {
 		fmt.Printf("Plan: %v\n", plan.Name)
 	}
-	params := selectParameters(plan)
-	extraVars, err := createExtraVars(ns, &params, plan)
+
+	params, err := selectParameters(plan)
+	if err != nil {
+		log.Errorf("Error validating selected parameters: %v", err)
+		return
+	}
+	extraVars, err := createExtraVars(execNamespace, &params, plan)
 	if err != nil {
 		log.Errorf("Error creating extravars: %v\n", err)
 		return
@@ -76,8 +85,7 @@ func RunBundle(action string, ns string, args []string) {
 		Location:   ns,
 		ExtraVars:  extraVars,
 	}
-	//	conf := runtime.Configuration{}
-	//	runtime.NewRuntime(conf)
+
 	k8scli, err := clients.Kubernetes()
 	if err != nil {
 		panic(err.Error())
@@ -135,31 +143,58 @@ func selectPlan(spec *bundle.Spec) bundle.Plan {
 	return bundle.Plan{}
 }
 
-func selectParameters(plan bundle.Plan) bundle.Parameters {
+func selectParameters(plan bundle.Plan) (bundle.Parameters, error) {
+	schemaPlan, err := bundle.ConvertPlansToSchema([]bundle.Plan{plan})
+	if err != nil {
+		log.Errorf("Error converting bundle plans to JSON Schema: %v", err)
+		return nil, err
+	}
+	planSchema := schemaPlan[0].Schemas
+	schemaParams := planSchema.ServiceInstance.Create["parameters"]
 	params := bundle.Parameters{}
 	for _, param := range plan.Parameters {
-		var paramDefault string
 		var paramInput string
+		var paramDefault interface{}
 		if param.Default != nil {
-			paramDefault = param.Default.(string)
+			paramDefault = param.Default
 		}
 		check := 1
 		for check < 2 {
 			fmt.Printf("Enter value for parameter [%v], default: [%v]: ", param.Name, paramDefault)
 			fmt.Scanln(&paramInput)
 			if paramInput == "" {
-				paramInput = paramDefault
+				switch paramDefault.(type) {
+				case int:
+					paramInput = strconv.Itoa(paramDefault.(int))
+				case string:
+					paramInput = paramDefault.(string)
+				case float64:
+					paramInput = strconv.FormatFloat(paramDefault.(float64), 'f', 0, 32)
+				}
 			}
 			if param.Required == true && paramInput == "" {
 				fmt.Printf("Parameter [%v] is required. Please try again.\n", param.Name)
+				continue
+			}
+
+			input, err := pruneInput(paramInput, param)
+			if err != nil {
+				fmt.Printf("Error accepting input: %v\n", err)
+				fmt.Println("Please try again")
 			} else {
 				check = 2
+				params.Add(param.Name, input)
 			}
 		}
-		params.Add(param.Name, paramInput)
 	}
+	v := validator.New(schemaParams)
+	if err := v.Validate(params); err != nil {
+		log.Debugf("Error validating parameters: %v", err)
+		return nil, err
+	}
+
 	log.Debugf("Params: %v\n", params)
-	return params
+	return params, nil
 }
 
 func createPodEnv(executionContext runtime.ExecutionContext) []v1.EnvVar {
@@ -203,4 +238,28 @@ func createExtraVars(targetNamespace string, parameters *bundle.Parameters, plan
 	paramsCopy["in_cluster"] = false
 	extraVars, err := json.Marshal(paramsCopy)
 	return string(extraVars), err
+}
+
+func pruneInput(input string, param bundle.ParameterDescriptor) (interface{}, error) {
+	var output interface{}
+	var err error
+	switch param.Type {
+	case "string":
+		output = input
+	case "enum":
+		output = input
+	case "bool":
+		output, err = strconv.ParseBool(input)
+		if err != nil {
+			return nil, errors.New("Input must be a boolean")
+		}
+	case "int":
+		output, err = strconv.ParseInt(input, 0, 0)
+		if err != nil {
+			return nil, errors.New("Input must be an integer")
+		}
+	default:
+		output = input
+	}
+	return output, nil
 }
