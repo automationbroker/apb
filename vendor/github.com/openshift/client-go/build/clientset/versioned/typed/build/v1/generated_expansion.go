137821ab36a650836be50ee1d18b3069d179115f
Dylan Murray <dymurray@redhat.com>
2018-07-11 15:02:08 -0400

    Update vendor to pick up bundle-lib release (#83)

--- vendor/github.com/openshift/client-go/build/clientset/versioned/typed/build/v1/generated_expansion.go
+++
@@ -1,5 +0,0 @@
-package v1
-
-type BuildExpansion interface{}
-
-type BuildConfigExpansion interface{}
--- vendor/github.com/openshift/client-go/template/clientset/versioned/typed/template/v1/generated_expansion.go
+++
@@ -1,7 +0,0 @@
-package v1
-
-type BrokerTemplateInstanceExpansion interface{}
-
-type TemplateExpansion interface{}
-
-type TemplateInstanceExpansion interface{}
