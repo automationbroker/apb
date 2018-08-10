06b9bee0dbe0d5c75957a8f4df461fe09a1f7682
Dylan Murray <dymurray@redhat.com>
2018-08-10 16:16:59 -0400

    Bug 1613224 - Add CLI param support for Runner field

--- cmd/registry.go
+++ cmd/registry.go
@@ -101,6 +101,7 @@ func init() {
 	// Registry Add Flags
 	registryAddCmd.Flags().StringVarP(&registryConfig.Config.Type, "type", "t", "dockerhub", "registry type (dockerhub, local_openshift, helm)")
 	registryAddCmd.Flags().StringVar(&registryConfig.Config.Org, "org", "", "organization for 'dockerhub' adapter to search (e.g. 'ansible-playbook-bundle')")
+	registryAddCmd.Flags().StringVar(&registryConfig.Config.Runner, "runner", "", "base image used to run Helm APBs (e.g. 'docker.io/automationbroker/helm-runner:latest')")
 	registryAddCmd.Flags().StringVar(&registryConfig.Config.URL, "url", "", "URL (e.g. docker.io)")
 	registryAddCmd.Flags().StringSliceVar(&registryConfig.Config.WhiteList, "whitelist", []string{}, "regexes for filtering registry contents (e.g. '.*apb$,.*bundle$')")
 	registryAddCmd.Flags().StringSliceVar(&registryConfig.Config.Namespaces, "namespaces", []string{}, "namespaces for 'local_openshift' adapter to search  (e.g. 'openshift,my-project')")
@@ -173,6 +174,9 @@ func applyOverrides(conf *registries.Config, params registries.Config) {
 	}
 	if params.URL != "" {
 		conf.URL = params.URL
+	}
+	if params.Runner != "" {
+		conf.Runner = params.Runner
 	}
 	if len(params.Namespaces) > 0 {
 		conf.Namespaces = params.Namespaces
